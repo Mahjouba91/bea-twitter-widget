3655bf129ff16117151df4e9f4b44dc56fc1b450
Benjamin Niess <benjamin.niess@gmail.com>
2013-07-08 18:27:09 +0200

    remove test function

--- classes/main.php
+++ classes/main.php
@@ -2,10 +2,6 @@
 class BEA_TW_Main {
 
 	public function __construct() {
-		add_action( 'init', array( __CLASS__, 'init' ) );
 	}
 	
-	public static function init() {
-		BEA_TW_Base::get_twitter_instance();
-	}
 }
